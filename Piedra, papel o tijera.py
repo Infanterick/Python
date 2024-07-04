fcc243ede3bb2441dc63524f5207b3581b4ce674
Erick Infante <infanterick1@gmail.com>
2024-07-04 17:36:45 -0400

    Cambios de código; uso de funciones para cada parte del juego

--- Piedra, papel o tijera.py
+++ Piedra, papel o tijera.py
@@ -4,51 +4,71 @@ Fecha: 22-06-2024
 Autor: Erick Infante
 *************************************************"""
 
-import random
-options = ("piedra", "papel", "tijera")
-user_wins = 0
-computer_wins = 0
-rounds = 1
-
 print("""
-      [ 🤖 Bienvenido al juego Piedra, Papel o tijera 🙋]
-                  >>> Ingresa una opción <<<
-      """)
+*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+/*/   Piedra, papel o tijera   /*/
+*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+""")
 
+import random
 
-while user_wins <2 and computer_wins <2:
-    
-    print("*" * 10)
-    print("ROUND # ", rounds)
-    print("*" * 10,"\n")
-    
+def choose_options():
+    options = ("piedra", "papel", "tijera")
     computer_option = random.choice(options)
-    user_option = input("Elija una opción (piedra, papel o tijera)\n=> ").lower()
+    user_option = input("Elija una opción (piedra, papel o tijera) => ").lower()
 
-    print(f"\nEl computador eligió {computer_option}.")
-    print(f"Tu elegiste {user_option}.\n")
-
-    if user_option == computer_option:
-        print("Empate...!!!\n")
-    elif user_option not in options:
-        print("El valor ingresado no es correcto;\npor favor elija una opción correcta \
+    if user_option not in options:
+        print("\nEl valor ingresado no es correcto;\npor favor elija una opción correcta \
             \n(Piedra, papel o tijera)\n")
-        continue
-    elif user_option == "piedra" and computer_option == "tijera" or \
-        user_option == "tijera" and computer_option == "papel" or \
-        user_option == "papel" and computer_option == "piedra":
-        print(f"Has ganado...!!! \n{user_option.capitalize()} le gana a {computer_option}.\n")
-        user_wins += 1
-    else:
-        print(f"Perdiste...!!! \n{computer_option.capitalize()} le gana a {user_option}.\n")    
-        computer_wins +=1
-    rounds +=1
-    print("Computador:", computer_wins, "veces.")
-    print("usuario:", user_wins, "veces.\n")
+        return None, None
+    
+    print(f"\nEl computador eligió: {computer_option}.")
+    print(f"Tu elegiste: {user_option}.\n")
+    return user_option, computer_option
 
-if computer_wins == 2:
-    print("LO SIENTO, EL COMPUTADOR HA GANADO...!!!")
-else:
-    print("FELICITACIONES, LE HAS GANADO AL COMPUTADOR...!!!")
 
-# Fin
+def check_rules(user_option, computer_option, user_wins, computer_wins):
+        
+        if user_option == computer_option:
+            print("Empate...!!!\n")
+       
+        elif user_option == "piedra" and computer_option == "tijera" or \
+            user_option == "tijera" and computer_option == "papel" or \
+            user_option == "papel" and computer_option == "piedra":
+            print(f"Has ganado...!!! {user_option.capitalize()} le gana a {computer_option}.\n")
+            user_wins += 1
+       
+        else:
+            print(f"Perdiste...!!! {computer_option.capitalize()} le gana a {user_option}.\n")    
+            computer_wins +=1
+       
+        return user_wins, computer_wins
+
+
+def run_game():
+
+    user_wins = 0
+    computer_wins = 0
+    rounds = 1
+
+    while user_wins <2 and computer_wins <2:
+        
+        print("*" * 20)
+        print("ROUND # ", rounds,"\n")
+        print("Computador:", computer_wins)
+        print("usuario:", user_wins)        
+        print("*" * 20,"\n")
+    
+        rounds +=1
+
+        user_option, computer_option = choose_options()
+        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+
+        if computer_wins == 2:
+            print("LO SIENTO, EL COMPUTADOR HA GANADO...!!!\n")
+            break
+        if user_wins == 2:
+            print("FELICITACIONES, LE HAS GANADO AL COMPUTADOR...!!!\n")
+            break
+
+run_game()
